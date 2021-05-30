0569c107423ba3313dde5cc6a928094131c43b8f
anlnt165 <53924921+anlnt165@users.noreply.github.com>
2021-05-29 20:08:42 -0400

    Javascript of koalabears

---
+++ sym-koalabear.js
@@ -0,0 +1,22 @@
+(functuon (PV) {
+    "use strict";
+
+    function symbolVis() {};
+    PV.deriveVisualizationFromBase(symbolVis);
+
+    var definition = {
+        typename: "koalabear"
+        visObjectType: symbolVis,
+        datasourceBehavior: PV.Extensibility.Enums.DatasoureBehaviors.Single,
+        getDefaultConfig: function(){
+            return {
+                Height: 150
+                Width: 150
+            }
+        }
+    }
+
+    symbolVis.prototype.init = function(scope, elem) { };
+
+    PV.symbolCatalog.register(definition);
+})(window.PIVisialization);
