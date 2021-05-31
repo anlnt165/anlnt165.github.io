f2e4bd6ccf686f32446093fb3d0dd334cfa3ee2f
anlnt165 <53924921+anlnt165@users.noreply.github.com>
2021-05-31 17:47:55 -0400

    Attempt to return the image from html file in JavaScript File

--- sym-koalabear.js
+++ sym-koalabear.js
@@ -8,11 +8,13 @@
         typename: "koalabear",
         visObjectType: symbolVis,
         datasourceBehavior: PV.Extensibility.Enums.DatasoureBehaviors.Single,
-        getDefaultConfig: function(){
-            return {
+        getDefaultConfig: function() {
+            const img = document.getElementById("koalabears").innerHTML;
+            return img
+            /* return {
                 Height: 150,
                 Width: 150
-            }
+            } */
         }
     }
 
